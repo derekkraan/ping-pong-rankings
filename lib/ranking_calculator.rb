ff63957374308ee189038e3e81309d99819c3a88
Derek Kraan <derek.kraan@gmail.com>
2012-08-21 18:48:25 +0000

    Make algorithms configurable and change the standard algorithm parameters.

--- lib/ranking_calculator.rb
+++ lib/ranking_calculator.rb
@@ -1,5 +1,5 @@
 class RankingCalculator
   # This function updates the player objects.
-  def calculate(score1, score2, player1, player2)
+  def calculate(score1, score2, team1_player1, team1_player2, team2_player1, team2_player2)
   end
 end
