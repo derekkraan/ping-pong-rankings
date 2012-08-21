02b7d9f80860c037c56891b72a7a86926baf393f
MartijnSch <martijn.scheijbeler@gmail.com>
2012-08-21 20:53:30 +0200

    Make score accessible

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :score
   has_many :games_as_t1p1, foreign_key: 'team1_player1_id', class_name: 'Game'
   has_many :games_as_t1p2, foreign_key: 'team1_player2_id', class_name: 'Game'
   has_many :games_as_t2p1, foreign_key: 'team2_player1_id', class_name: 'Game'
