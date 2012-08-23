df5e9dd40d10e05cb5b5fe406dd6c59dced7555b
Derek Kraan <derek.kraan@gmail.com>
2012-08-23 14:29:02 +0200

    Fix bug reset_score -> reset_rating

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -4,7 +4,7 @@ class PlayerController < ApplicationController
 
   def save
     new_player = Player.new(params['player'])
-    new_player.reset_score
+    new_player.reset_rating
 
     view = new_player.save ? 'new' : 'save_fail'
 
