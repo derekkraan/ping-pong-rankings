7ad648fad75699b988834259cd5e69a465d4db0d
Derek Kraan <derek.kraan@gmail.com>
2012-12-02 14:49:57 +0100

    Use real twitter avatars, and fix twitter config to use env vars on heroku.

--- app/models/player.rb
+++ app/models/player.rb
@@ -49,4 +49,22 @@ class Player < ActiveRecord::Base
     initial.rating = ranking_algorithm::DEFAULT_PLAYER_RATING
     initial
   end
+
+  def default_profile_image_url
+    'http://a0.twimg.com/sticky/default_profile_images/default_profile_6_normal.png'
+  end
+
+  def profile_image_url(options={})
+    size = options.fetch(:bigger, false) ? '_bigger' : '_normal'
+    if twitter.present?
+      begin
+        Twitter.user(twitter).profile_image_url.gsub('_normal', size)
+      rescue
+        logger.debug "Twitter avatar fetch failed for User id: #{id}"
+        default_profile_image_url
+      end
+    else
+      default_profile_image_url
+    end
+  end
 end
